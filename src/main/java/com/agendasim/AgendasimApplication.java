43a51dd4670dab59d14fffb28799c43d8bf23136
cochitogupy <adriel.cochito@gupy.com.br>
2025-06-13 01:36:26 -0300

    first commit

---
+++ src/main/java/com/agendasim/AgendasimApplication.java
@@ -0,0 +1,13 @@
+package com.agendasim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendasimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendasimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendasim/AgendasimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendasim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendasimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
