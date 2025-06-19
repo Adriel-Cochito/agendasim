1ff235d85f16dc6143f57d399048a5df20dfc99e
Adriel Cochito <adrielcochito.dev@gmail.com>
2025-06-19 14:16:05 -0300

    remove import desuso

--- src/main/java/com/agendasim/model/Servico.java
+++ src/main/java/com/agendasim/model/Servico.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
-import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDateTime;
 
