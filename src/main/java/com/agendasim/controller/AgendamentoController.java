bc769c0d4bb5f72bd43d2eb54619dcbb9170d60d
Adriel Cochito <adrielcochito.dev@gmail.com>
2025-06-27 01:17:17 -0300

    Agenda Controller Serviços Lista com Profissionais

--- src/main/java/com/agendasim/controller/AgendamentoController.java
+++ src/main/java/com/agendasim/controller/AgendamentoController.java
@@ -1,5 +1,28 @@
 package com.agendasim.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.agendasim.model.Servico;
+import com.agendasim.service.ServicoService;
+
+@RestController
+@RequestMapping("/agendamentos")
 public class AgendamentoController {
+
+    @Autowired
+    private ServicoService servicoService;
+    
+    // LISTAR (sempre requer empresaId)
+    @GetMapping("/servicos")
+    public ResponseEntity<List<Servico>> listarPorEmpresa(@RequestParam Long empresaId) {
+        return ResponseEntity.ok(servicoService.listarPorEmpresa(empresaId));
+    }
 
 }
