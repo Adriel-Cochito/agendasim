94973ba0ea8e28604cea9df50a6667e45f9fb634
Adriel Cochito <adrielcochito.dev@gmail.com>
2025-06-28 14:45:15 -0300

    Perfis de Profissional

---
+++ src/main/java/com/agendasim/enums/Perfil.java
@@ -0,0 +1,7 @@
+package com.agendasim.enums;
+
+public enum Perfil {
+    OWNER,
+    ADMIN,
+    USER
+}
--- src/main/java/com/agendasim/model/Profissional.java
+++ src/main/java/com/agendasim/model/Profissional.java
@@ -1,13 +1,13 @@
 package com.agendasim.model;
 
-import java.time.LocalDateTime;
-
+import com.agendasim.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
@@ -31,6 +31,10 @@ public class Profissional {
     @Size(min = 3, max = 120, message = "A senha deve ter entre 6 e 120 caracteres")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Perfil perfil = Perfil.USER;
 
     @Size(max = 512, message = "O token do Google é muito grande")
     private String googleAccessToken;
